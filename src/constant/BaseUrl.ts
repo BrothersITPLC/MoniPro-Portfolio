98f05b413438cdcbe359a8a9f81ce3b936a8f01f
kidist31 <kidist.kifle@brothersitplc.com>
2025-08-15 20:06:42 +0300

    allow others telegram user to log in

--- src/constant/BaseUrl.ts
+++ src/constant/BaseUrl.ts
@@ -1,7 +1,6 @@
 /* DEV */
-
 // export const BaseUrl = "http://localhost:8000/api";
 
 /* STG */
 
-export const BaseUrl = "https://monipro.brothersit.dev/api";
+ export const BaseUrl = "https://monipro.brothersit.dev/api";
